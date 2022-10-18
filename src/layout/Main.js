ed5928969ae63588530df69db09cf6589148a7f2
Ashikzzzz <ashik.emon5.im@gmail.com>
2022-10-19 03:15:06 +0600

    compo ready

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
+// import Home from "../Pages/Home/Home/Home";
+import Header from "../Pages/Shared/Header/Header";
+import Left from "../Pages/Shared/Left/Left";
+import Right from "../Pages/Shared/Right/Right";
 
 const Main = () => {
   return (
     <div>
-      <Outlet></Outlet>
+      <Header></Header>
+      <Container>
+        <Row>
+          <Col lg="2">
+            <Left></Left>
+          </Col>
+          <Col lg="7">
+            <Outlet></Outlet>
+          </Col>
+          <Col lg="3">
+            <Right></Right>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
